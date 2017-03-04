acbfcbe91dc6e7d7fd4304e38e2545be81665e91
chengyurou <18829292695@163.com>
2017-03-04 12:14:57 +0800

    answer

--- fizz-buzz.rb
+++ fizz-buzz.rb
@@ -1,5 +1,18 @@
 class FizzBuzz
   def judgeFB(n)
+    array = []
 
+    (1..n).each do |num|
+      if num%3 == 0 && num%5==0 then
+        array.push "FizzBuzz"
+      elsif num%3 == 0 then
+        array.push "Fizz"
+      elsif num%5 == 0 then
+        array.push "Buzz"
+      else
+        array.push num.to_s
+      end
+    end
+    array
   end
 end
